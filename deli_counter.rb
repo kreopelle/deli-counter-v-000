2956f9654c256c3431e911b94ece12bef3ca30f3
Kayla Reopelle <kreopelle@gmail.com>
2018-04-08 17:50:27 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,10 @@
 def line(katz_deli)
   if katz_deli.size >= 1
+    customers_in_line = []
     katz_deli.each_with_index do |customer, index|
-      puts "The line is currently: "
+      customers_in_line << "#{index + 1}. #{customer}"
+    end
+    puts "The line is currently: #{customers_in_line.join(" ")}"
   else
     puts "The line is currently empty."
   end
