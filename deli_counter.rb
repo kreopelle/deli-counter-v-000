fd330e661dc8b197ed254106f950737d26ea6a4b
Kayla Reopelle <kreopelle@gmail.com>
2018-04-08 17:47:53 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,7 @@
 def line(katz_deli)
   if katz_deli.size >= 1
-
+    katz_deli.each_with_index do |customer, index|
+      puts "The line is currently: "
   else
     puts "The line is currently empty."
   end
