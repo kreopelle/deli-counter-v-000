1d89409141c70231764c14c850c0077fe6ce31a3
Kayla Reopelle <kreopelle@gmail.com>
2018-04-08 17:57:40 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,3 +14,11 @@ def take_a_number(katz_deli, new_customer)
   katz_deli << new_customer
   puts "Welcome, #{new_customer}. You are number #{katz_deli.size} in line."
 end
+
+def now_serving(katz_deli)
+  if katz_deli.size >= 1
+    puts "Currently serving #{katz_deli[0]}."
+  else
+    puts "There is nobody waiting to be served!"
+  end
+end
